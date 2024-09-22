1014fb74b8f963fa7ca034e87cd2b337240bc507
Sifiso <shishabas@invokesolutions.co.za>
2024-09-22 14:39:05 +0200

    Add project files.

---
+++ Controllers/IdentityController.cs
@@ -0,0 +1,34 @@
+﻿using ImageProcessor.Dto.Request;
+using ImageProcessor.Dto.Response;
+using ImageProcessor.Exceptions;
+using ImageProcessor.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ImageProcessor.Controllers;
+
+[ApiController]
+[Route("api/v1/identity")]
+public class IdentityController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public IdentityController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("login")]
+    [ProducesResponseType(typeof(LoginResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Auth([FromBody] LoginRequest request)
+    {
+        try
+        {
+            return Ok(await _authService.Login(request));
+        }
+        catch (GenericUserException e)
+        {
+            return Unauthorized(e.Message);
+        }
+    }
+}
---
+++ Controllers/ImageController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ImageProcessor.Dto.Request;
+using ImageProcessor.Dto.Response;
+using ImageProcessor.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+
+namespace ImageProcessor.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/v1/image")]
+public class ImageController : ControllerBase
+{
+    private readonly IimageService _imageService;
+
+    public ImageController(IimageService imageService)
+    {
+        _imageService = imageService;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(OrderResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> ProcessOrder([FromBody] ProcessOrderRequest request)
+    {
+        return Ok(await _imageService.ProcessOrder(request));
+    }
+
+    [HttpGet("{Id}")]
+    [ProducesResponseType(typeof(QueueStatus), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetStatus([FromRoute] string Id)
+    {
+        return Ok(await _imageService.GetQueueStatus(Id));
+    }
+}
